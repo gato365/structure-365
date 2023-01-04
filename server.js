4a176007f98e20be9c07d3a0a0843d848e56ab21
gato365 <immanuelwilliams@gato365.com>
2023-01-04 10:46:06 -0800

    Made handlebars work using server and routes

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const {engine} = require("express-handlebars");
 
 //defaults to index.js
 const mainRouter = require("./controllers");
 
-const app = express ();
+// Creating the App
+const app = express();
 
-
+// Either on Heroku's port or my local port 
 const PORT = process.env.PORT || 3001;
 
+
+// Use the main router
+app.engine("handlebars",engine());
+app.set('view engine', 'handlebars');
 app.use(mainRouter);
 
 
