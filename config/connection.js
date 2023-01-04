452eef54b2218b17446fd6919a174bde03a5b3f8
gato365 <immanuelwilliams@gato365.com>
2023-01-04 12:08:03 -0800

    Made connect between handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,35 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+// let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     }
+//   );
+// }
+
+// module.exports = sequelize;
+
+
+
+const sequelize = new Sequelize(
+  process.env.DB_DB,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+);
 
 module.exports = sequelize;
