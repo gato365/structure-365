c82b4dba7a4e5a31dcae7dd64ccdf54bed32db30
gato365 <immanuelwilliams@gato365.com>
2023-01-05 20:59:23 -0800

    Changing code to work well (not good commit)

--- config/connection.js
+++ config/connection.js
@@ -3,33 +3,22 @@ require('dotenv').config();
 
 // let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
-// }
-
-// module.exports = sequelize;
-
-
-
-const sequelize = new Sequelize(
-  process.env.DB_DB,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-  },
-);
+// **** This is for Heroku
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_DB,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
+
+
