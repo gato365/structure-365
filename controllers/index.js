c5fbc2a63c982f1aef223cf68385127dd4894bf4
gato365 <immanuelwilliams@gato365.com>
2023-01-05 19:28:16 -0800

    Added proper routes to controllers index

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,12 @@
 const { Router } = require ("express");
 
-const pathRouter = require("./pathRouter");
+const pathRouter = require('./pathRouter');
+const apiRouter = require("./apis");
 
 const allRouter = new Router();
 
-
-allRouter.use("/", pathRouter);
+allRouter.use('/', pathRouter);
+allRouter.use('/api', apiRouter);
 
 module.exports = allRouter;
 
