c0e66bdc86b028a3c826846adba82bb108ee3886
gato365 <immanuelwilliams@gato365.com>
2023-01-07 15:09:10 -0800

    Magic was created power Not added lol

--- controllers/apis/exercise.js
+++ controllers/apis/exercise.js
@@ -53,6 +53,33 @@ exerciseRouter.get('/', async (req, res) => {
 
 
 
+
+function addPowerSets(tmpPowerJSON) {
+
+  // Find Length of JSON
+  const numberSets = tmpPowerJSON.length;
+  // Empty Array for Powers per set
+  const calculatedPower = [];
+
+  // Calculate Power for all sets
+  for (let i = 0; i < numberSets; i++) {
+    let setPower = tmpPowerJSON[i].reps * tmpPowerJSON[i].weight;
+    calculatedPower.push(setPower);
+  }
+
+
+  // Find the sum of Power for set
+  let sumPower = 0;
+  for (let i = 0; i < numberSets; i++) {
+    sumPower += calculatedPower[i];
+  }
+
+
+  return sumPower;
+}
+
+
+
 // 3) Get 1 exercise
 exerciseRouter.get('/:id', async (req, res) => {
 
@@ -60,62 +87,32 @@ exerciseRouter.get('/:id', async (req, res) => {
 
   try {
     const exerciseData = await Exercise.findByPk(req.params.id, {
-      // Need to do a task, I am pretty SUre
-      // include:  ['powerInfo'],
+
+
+      
+
     });
 
 
-
-
-
-    function addsum(arr) {
-      var sum = 0;
-      for (var z = 0; z < arr.length; z++) {
-        sum += arr[z];
-      }
-      return sum;
-    }
 
     const powerData = await Exercise.findByPk(req.params.id, {
       attributes: ['powerInfo'],
     });
+    const calcultedPower = addPowerSets(powerData.powerInfo);
 
+    exerciseData.powerBig = calcultedPower;
 
-
-
-
-
-
-
-
-    const numberSets = powerData.powerInfo.length;
-    const calculatedPower = [];
-
-    // Calculate Power for all sets
-    for (let i = 0; i < numberSets; i++) {
-      let setPower = powerData.powerInfo[i].reps * powerData.powerInfo[i].weight;
-      calculatedPower.push(setPower);
-    }
-
-
-    console.log(calculatedPower);
-
-
-
-    // Attempt 0: Just as it is (an JSON Object?)
-    // console.log(powerData);
-    // Attempt 1: Turns into a string
-    // console.log(JSON.stringify(powerData));
-    // Attempt 2: Get First element from JSON (Does not work-undefined)
-    // console.log(powerData[0]);
-    // Attempt 3: Parse object
-    // console.log(JSON.parse(powerData));
+    console.log(exerciseData)
+    
+    
 
 
     if (!exerciseData) {
       res.status(404).json({ message: 'No exercise found with this id!' });
       return;
     }
+
+
 
 
     // Print to Screen
