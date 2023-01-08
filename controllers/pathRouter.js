5e643c9396453a2653ebc824b63ab97ac3720333
Ahneb <benjaminha1996@gmail.com>
2023-01-07 23:28:01 -0800

    added login button functionality to go to home signup_page added jwt key check on login to verify user added functionality to signup button on login page to redirect to signup page

--- controllers/pathRouter.js
+++ controllers/pathRouter.js
@@ -1,28 +1,47 @@
 const { Router } = require("express");
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
 
 const pathRouter = new Router();
 
-pathRouter.get('/', (req, res) => {
-    res.render('home', {
+pathRouter.get('/', async (req, res) => {
+    const { logintoken } = req.cookies;
 
-        // ~~~~~~~~~~~~~~~~~~ TEST ~~~~~~~~~~~~~~~~~~~//
-        chartData: [
-            { x: 50, y: 7 },
-            { x: 60, y: 8 },
-            { x: 70, y: 8 }, 
-            { x: 80, y: 9 },
-            { x: 90, y: 9 },
-            { x: 100, y: 9 },
-            { x: 110, y: 10 },
-            { x: 120, y: 11 },
-            { x: 130, y: 14 },
-            { x: 140, y: 14 },
-            { x: 150, y: 15 }
+    try {
+        const data = jwt.verify(logintoken, process.env.JWT_KEY);
+        const { id } = data;
 
-        ]
-        // ~~~~~~~~~~~~~~~~~~ TEST ~~~~~~~~~~~~~~~~~~~//
+        const user = await User.findByPk(id);
+        const plainUser = user.get({plain: true});
 
-    });
+        res.render('home', {
+            user: plainUser,
+            // ~~~~~~~~~~~~~~~~~~ TEST ~~~~~~~~~~~~~~~~~~~//
+            chartData: [
+                { x: 50, y: 7 },
+                { x: 60, y: 8 },
+                { x: 70, y: 8 }, 
+                { x: 80, y: 9 },
+                { x: 90, y: 9 },
+                { x: 100, y: 9 },
+                { x: 110, y: 10 },
+                { x: 120, y: 11 },
+                { x: 130, y: 14 },
+                { x: 140, y: 14 },
+                { x: 150, y: 15 }
+    
+            ]
+            // ~~~~~~~~~~~~~~~~~~ TEST ~~~~~~~~~~~~~~~~~~~//
+        });
+    } catch(error) {
+        if(error.message === "invalid token" || error.message === "jwt must be provided") {
+            res.redirect('/login');
+        } else {
+            res.status(500).end("bad things happened here");
+        }
+    }
+    
 
 })
 
