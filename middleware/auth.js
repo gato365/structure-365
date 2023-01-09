d64015c86a8b2544303d7f38e20298060cdc3641
Piercen Helms <phelms1999@gmail.com>
2023-01-08 20:00:09 -0800

    added auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+
 const User = require('../models/User');
 
 module.exports = async (req, res, next) => {
@@ -10,15 +11,20 @@ module.exports = async (req, res, next) => {
         const { id } = data;
 
         const user = await User.findByPk(id);
+        if (!user) {
+            res.redirect('/login');
+            return;
+        }
 
         req.user = user;
         next();
     }  catch(error) {
-        if(error.message === "invalid token" || error.message === "jwt must be provided") {
-            res.redirect('/login');
+        if(error.message === "invalid token") {
+            res.redirect('/landing');
+        } else if (error.message === "jwt must be provided") {
+           res.redirect('/login');
         } else {
             res.status(500).end("bad things happened here");
         }
     }
-
-    }
+}
